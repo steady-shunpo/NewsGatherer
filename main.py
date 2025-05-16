67332918614a8b5d9e047ef69e4cee2c3fb955b9
steady-shunpo <shunpoguy@gmail.com>
2025-05-17 00:25:34 +0530

    first

---
+++ main.py
@@ -0,0 +1,14 @@
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import asyncio
+import os
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+)
